e6576855287e8538f8b195429ec89425ca1d56b9
Basang Basangov <m300609@mail.ru>
2024-12-30 14:58:10 +0300

    ASTONJS-6 [feat] Assignment 6

---
+++ Assignment 6/basic.js
@@ -0,0 +1,79 @@
+// 1
+// console.log("1");
+// setTimeout(() => console.log("2"), 1);
+// let promiseNew = new Promise((resolve) => {
+//   console.log("3");
+//   resolve();
+// });
+// promiseNew.then(() => console.log("4"));
+// setTimeout(() => console.log("5"));
+// console.log("6");
+// Output: 1 3 6 4 5 2
+
+// 2
+// let promiseTree = new Promise((resolve, reject) => {
+//   resolve("a");
+//   console.log("1");
+//   setTimeout(() => {
+//     console.log("2");
+//   }, 0);
+//   console.log("3");
+// });
+// Output: 1 3 2
+
+// 3
+// let promiseTwo = new Promise((resolve, reject) => {
+//   resolve("a");
+// });
+// promiseTwo
+//   .then((res) => {
+//     return res + "b";
+//   })
+//   .then((res) => {
+//     return res + "с";
+//   })
+//   .finally((res) => {
+//     return res + "!!!!!!!";
+//   })
+//   .catch((res) => {
+//     return res + "d";
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+// Output: abc
+
+// 4
+// function doSmth() {
+//   return Promise.resolve("123");
+// }
+// doSmth()
+//   .then(function (a) {
+//     console.log("1", a); //
+//     return a;
+//   })
+//   .then(function (b) {
+//     console.log("2", b);
+//     return Promise.reject("321");
+//   })
+//   .catch(function (err) {
+//     console.log("3", err);
+//   })
+//   .then(function (c) {
+//     console.log("4", c);
+//     return c;
+//   });
+// Output:
+// 1 123
+// 2 123
+// 3 321
+// 4 undefined
+
+// 5
+console.log("1");
+setTimeout(function () {
+  console.log("2");
+}, 0);
+Promise.resolve().then(() => console.log("3"));
+console.log("4");
+// Output: 1 4 3 2
